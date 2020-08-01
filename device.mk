e311019626996730da27c4bf5cbabab1a4e5c53a
baunilla <49725678+baunilla@users.noreply.github.com>
2020-08-01 22:03:43 +0600

    riva: Import custom LiveDisplay 2.0 HAL
    Pulled from LineageOS/android_hardware_lineage_livedisplay/sdm
    
    Cleaned up and adapted for PictureAdjustment (vendor) only:
    
    * Removed AdaptiveBacklight, DisplayModes, Constants, Types, Utils
    * Moved necessary constants to anonymous namespace
    
    Change-Id: Ie8421256c4d7a9413b605471299dc7d83f88a4b5
    Signed-off-by: erwinabs <erwinabubakarsidik@gmail.com>

--- device.mk
+++ device.mk
@@ -227,7 +227,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service.xiaomi_riva
 
 # Media
 PRODUCT_COPY_FILES += \
